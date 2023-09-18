af3a316ccb05c2cb9740244ae3d5d4b54bb8a5b2
Harrison Gertler <harrisongertler@mac.com>
2023-09-18 18:42:08 -0400

    WIP: Building UITableView for Airports

---
+++ ForeFlightExercise/ViewControllers/FFNavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FFNavigationViewController.swift
+//  ForeFlightExercise
+//
+//  Created by Harrison Gertler on 9/18/23.
+//
+
+import UIKit
+
+class FFNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ForeFlightExercise/Views/AirportTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AirportTableViewCell.swift
+//  ForeFlightExercise
+//
+//  Created by Harrison Gertler on 9/18/23.
+//
+
+import UIKit
+
+class AirportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
